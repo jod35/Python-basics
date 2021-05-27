66f4ce8d42f08f046a873f4b2180266d9051d040
jod35 <jodestrevin@gmail.com>
2021-05-27 13:12:53 +0300

    first commit

---
+++ comments.py
@@ -0,0 +1,4 @@
+print(";Python is cool") #cool
+#cool
+#we are learning Python
+print("This is cool") #cool
---
+++ numbers and arithmetic.py
@@ -0,0 +1,42 @@
+#Numbers and Arithmetic
+
+
+# + - plus (addition)
+# - - minus (subtraction)
+# * - asteric (multiplication)
+# / - divide  (division)
+# % - modulus (finding a remainder)
+# > - greater than
+# < - less than
+# >= - greater than or equal to
+# <= - less than or equal to
+
+1,2,3,4,5, #integers
+
+1.2,1.4,5.67 #floating numbers
+
+# print(2+3)
+
+# print(10-2)
+
+# print(25*4)
+
+# print(1000/25)
+
+# print(5%2)
+
+# print(25>0)
+# print(34>100)
+
+# print(25<0)
+# print(34<100)
+
+
+# print(25>=0)
+# print(25>=100)
+
+
+print(25<=0)
+print(25<=100)
+
+
